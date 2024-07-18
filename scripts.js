b65ca7d0b1944c7f981c798b70fe634e8fe6d24f
VertigoCool99 <83370667+VertigoCool99@users.noreply.github.com>
2024-07-18 20:27:43 +0100

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,27 +1,8 @@
 document.addEventListener("DOMContentLoaded", function() {
-    // Smooth scrolling for links
-    function smoothScroll(link) {
-        link.addEventListener('click', function(e) {
-            e.preventDefault();
-            var targetId = this.getAttribute('href').substring(1);
-            var targetElement = document.getElementById(targetId);
-            if (targetElement) {
-                targetElement.scrollIntoView({
-                    behavior: 'smooth'
-                });
-            }
-        });
-    }
+    function highlightActiveLink() {
+        var sidebarLinks = document.querySelectorAll('.sidebar-link');
+        var sections = document.querySelectorAll('.content-section');
 
-    var tocLinks = document.querySelectorAll('.toc-link');
-    var sidebarLinks = document.querySelectorAll('.sidebar-link');
-    var sections = document.querySelectorAll('.content-section');
-
-    tocLinks.forEach(smoothScroll);
-    sidebarLinks.forEach(smoothScroll);
-
-    // Highlight active section in sidebar and TOC
-    function updateActiveLink() {
         var scrollPosition = window.scrollY + 150; // Adjust scroll position for active link
 
         sections.forEach(function(section) {
@@ -34,14 +15,10 @@ document.addEventListener("DOMContentLoaded", function() {
                 sidebarLinks.forEach(function(link) {
                     link.classList.toggle('active', link.getAttribute('href').includes(sectionId));
                 });
-
-                tocLinks.forEach(function(link) {
-                    link.classList.toggle('active', link.getAttribute('href').includes(sectionId));
-                });
             }
         });
     }
 
-    window.addEventListener('scroll', updateActiveLink);
-    updateActiveLink(); // Initial call to set the correct active link on page load
+    window.addEventListener('scroll', highlightActiveLink);
+    highlightActiveLink(); // Initial call to set the correct active link on page load
 });
